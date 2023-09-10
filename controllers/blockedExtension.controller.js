af0942446ce0f1667401bf0aa2f48f96af96d2f1
junseok1925 <rkdwnstjr1925@naver.com>
2023-09-10 23:13:47 +0900

    [fix] 커스텀 확장자명 영문만 입력가능, [Fixes] 파일 확장자명을 일부러 수정해서 업로드시 차단(해결중)

--- controllers/blockedExtension.controller.js
+++ controllers/blockedExtension.controller.js
@@ -1,7 +1,6 @@
 const blockedExtensionService = require("../services/blockedExtension.service");
 
 class BlockedExtensionController {
-
   // 차단된 확장자 확인
   getAllExtensions = async (req, res) => {
     try {
@@ -9,7 +8,7 @@ class BlockedExtensionController {
       res.status(200).json({ data: extension });
     } catch (error) {
       console.error(error);
-      return res.status(500).json({ errMsg: '확장자 조회 실패.' });
+      return res.status(500).json({ errMsg: "확장자 조회 실패." });
     }
   };
 
@@ -17,17 +16,23 @@ class BlockedExtensionController {
   addExtension = async (req, res) => {
     try {
       const { name } = req.body;
+      // name 값이 없거나 영어 알파벳이 아닌 경우의 검사
+      if (!name || !/^[A-Za-z]+$/.test(name)) {
+        return res.status(400).json({
+          errMsg: "차단할 확장자명을 한글이 아닌 영문으로 입력해주세요",
+        });
+      }
       const extension = await blockedExtensionService.addExtension(name);
       res.status(200).json({ data: extension });
-      if(!name){
-        res.status(400).json('차단할 확장자명을 입력해주세요.')
+      if (!name) {
+        res.status(400).json("차단할 확장자명을 입력해주세요.");
       }
     } catch (error) {
-      if (error.message === '이미 등록된 확장자') {
+      if (error.message === "이미 등록된 확장자") {
         return res.status(400).json({ errMsg: error.message });
       }
       console.error(error);
-      return res.status(500).json({ errMsg: '확장자 등록 실패.' });
+      return res.status(500).json({ errMsg: "확장자 등록 실패." });
     }
   };
 
@@ -36,19 +41,19 @@ class BlockedExtensionController {
     try {
       const { name } = req.params;
       if (!name) {
-        return res.status(400).json({ errMsg: '값없음 : name' });
+        return res.status(400).json({ errMsg: "값없음 : name" });
       }
       const deleteResult = await blockedExtensionService.deleteExtension(name);
       if (deleteResult) {
-        return res.status(200).json({ msg: '삭제성공' });
+        return res.status(200).json({ msg: "삭제성공" });
       } else {
-        return res.status(400).json({ errMsg: '삭제실패' });
+        return res.status(400).json({ errMsg: "삭제실패" });
       }
     } catch (err) {
       console.error(err);
-      return res.status(500).json({ errMsg: '전체에러' });
+      return res.status(500).json({ errMsg: "전체에러" });
     }
-  }
+  };
 }
 
 module.exports = new BlockedExtensionController();
